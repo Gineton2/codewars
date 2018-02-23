036e906b08cde3fe06f41e82334b46e493b1b4ff
Gineton Alencar II <gineton2@gmail.com>
2018-02-22 22:16:06 -0800

    Used TreeSet instead of ArrayList.
    Eliminates need for sorting. Only works with lowercase Strings.

--- WhichAreIn/WhichAreIn.java
+++ WhichAreIn/WhichAreIn.java
@@ -1,20 +1,17 @@
 import java.util.*;
-import static org.junit.Assert.*;
-import org.junit.Test;
 
 public class WhichAreIn { 
     public static String[] inArray(String[] array1, String[] array2) {
-       List<String> results = new ArrayList<String>();
+       Set<String> results = new TreeSet<String>();
         for (String a1 : array1) {
             for (String a2 : array2) {
                 if (a2.contains(a1)) {
-                    results.add(a);
+                    results.add(a1);
+                    System.out.println(a1);
                     break;
                 }
             }
         }
-        Collections.sort(results);
-        String[] r = results.toArray(new String[results.size()]);
-        return r;
+        return results.toArray(new String[results.size()]);
     }
 }
